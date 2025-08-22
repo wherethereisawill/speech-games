e27365ef71f59be754828448c6119a82305e9680
Will Norris <will.norris@hey.com>
2025-08-22 21:46:35 +0100

    add

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,21 @@
 # uvicorn main:app --reload --host 0.0.0.0 --port 8000
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
